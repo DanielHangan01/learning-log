a224fe47851ac3e56d25a703ad194c7c6e997c06
DanielHangan01 <danhangan@hotmail.com>
2023-11-09 04:33:29 +0100

    user logout

--- users/urls.py
+++ users/urls.py
@@ -7,4 +7,7 @@ from . import views
 urlpatterns = [
     # Login page
     path('login/', LoginView.as_view(template_name='users/login.html'), name="login"),
+
+    # Logout page
+    path('logout/', views.logout_view, name='logout'),
 ]
